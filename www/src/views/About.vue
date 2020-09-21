34b68f76fd820b23d787c6279146ad562b5fa662
abdon-copper <59224618+abdon-copper@users.noreply.github.com>
2020-09-21 21:03:15 +0200

    About links to github

--- www/src/views/About.vue
+++ www/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>merge-jwl</h1>
+    <a href="https://github.com/abdon-gadgets/merge-jwl">Source code at Github</a>
   </div>
 </template>
